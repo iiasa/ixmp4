19ffcdbaa583a538fd3c4ad042beffbcc251ef35
Max Wolschlager <comms@meks.io>
2023-09-14 16:16:12 +0200

    regress tests

--- tests/data/test_model.py
+++ tests/data/test_model.py
@@ -70,6 +70,9 @@ def test_tabulate_model(test_mp):
 def test_filter_model(test_mp):
     run1, _ = create_filter_test_data(test_mp)
 
+    res = test_mp.backend.models.tabulate(name__like="Model %")
+    assert sorted(res["name"].tolist()) == ["Model 1", "Model 2"]
+
     res = test_mp.backend.models.tabulate(
         iamc={
             "region": {"name": "Region 1"},
