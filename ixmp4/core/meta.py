da143a07a0f251eaa55b4c18f3a21764297c066c
Daniel Huppmann <dh@dergelbesalon.at>
2023-11-24 06:22:24 +0100

    Use direct merge with runs-table

--- ixmp4/core/meta.py
+++ ixmp4/core/meta.py
@@ -1,19 +1,18 @@
 import pandas as pd
 
+from .run import RunRepository
 from .base import BaseFacade
 
 
 class MetaRepository(BaseFacade):
+    def __init__(self, **kwargs) -> None:
+        super().__init__(**kwargs)
+        self.runs = RunRepository(_backend=self.backend)
+
     def tabulate(self, **kwargs) -> pd.DataFrame:
         # TODO: accept list of `Run` instances as arg
-
-        runs = self.backend.runs.tabulate(**kwargs.get("run", {})).set_index("id")
-        meta = self.backend.meta.tabulate(**kwargs)
-
-        for column, mapper in [
-            ("model", self.backend.models.map()),
-            ("scenario", self.backend.scenarios.map()),
-        ]:
-            meta[column] = meta["run__id"].map(dict(runs[f"{column}__id"])).map(mapper)
-        meta["version"] = meta["run__id"].map(dict(runs["version"])).map(int)
+        runs = self.runs.tabulate(**kwargs.get("run", {}))
+        meta = self.backend.meta.tabulate(**kwargs).merge(
+            runs, left_on="run__id", right_on="id"
+        )
         return meta[["model", "scenario", "version", "key", "value"]]
