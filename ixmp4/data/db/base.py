c3db9754b52bb7d4bf9c3cf85e6d78c34595fc52
Max Wolschlager <comms@meks.io>
2024-02-29 14:30:02 +0100

    add some comments

--- ixmp4/data/db/base.py
+++ ixmp4/data/db/base.py
@@ -391,8 +391,12 @@ class BulkUpserter(BulkOperator[ModelType]):
             for col in self.model_class.updateable_columns:
                 updated_col = col + self.merge_suffix
                 if updated_col in df.columns:
+                    # coerce to same type so the inequality
+                    # operation works with pyarrow installed
                     df[updated_col] = df[updated_col].astype(df[col].dtype)
                     are_not_equal = df[col] != df[updated_col]
+                    # extra check if both values are NA because NA == NA = NA
+                    # in pandas with pyarrow
                     both_are_na = pd.isna(df[col]) & pd.isna(df[updated_col])
                     cond.append(~both_are_na | are_not_equal)
 
