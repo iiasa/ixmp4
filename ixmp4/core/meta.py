f2507c17618d5919459a5b9e05fca5e2339a7e83
Daniel Huppmann <dh@dergelbesalon.at>
2024-03-16 07:45:58 +0100

    Add filtering by model and scenario name in MetaRepository

--- ixmp4/core/meta.py
+++ ixmp4/core/meta.py
@@ -5,8 +5,7 @@ from .base import BaseFacade
 
 class MetaRepository(BaseFacade):
     def tabulate(self, **kwargs) -> pd.DataFrame:
-        # TODO: accept list of `Run` instances as arg
-        # TODO: expand run-id to model-scenario-version-id columns
+        # TODO: accept list of `Run` instances as argument
         return self.backend.meta.tabulate(join_run_index=True, **kwargs).drop(
             columns=["id", "type"]
         )
--- ixmp4/data/db/meta/filter.py
+++ ixmp4/data/db/meta/filter.py
@@ -1,8 +1,24 @@
+from typing import ClassVar
+
 from ixmp4.data.db import filters as base
+from ixmp4.data.db.model import Model
 from ixmp4.data.db.run import Run
+from ixmp4.data.db.scenario import Scenario
 from ixmp4.db import filters, utils
 
 from .model import RunMetaEntry
+
+
+class ModelFilter(base.RunFilter, metaclass=filters.FilterMeta):
+    name: filters.String
+
+    sqla_model: ClassVar[type] = Model
+
+
+class ScenarioFilter(base.RunFilter, metaclass=filters.FilterMeta):
+    name: filters.String
+
+    sqla_model: ClassVar[type] = Scenario
 
 
 class RunFilter(base.RunFilter, metaclass=filters.FilterMeta):
@@ -13,6 +29,8 @@ class RunFilter(base.RunFilter, metaclass=filters.FilterMeta):
 
 
 class RunMetaEntryFilter(base.RunMetaEntryFilter, metaclass=filters.FilterMeta):
+    model: ModelFilter
+    scenario: ScenarioFilter
     run: RunFilter = filters.Field(
         default=RunFilter(id=None, version=None, is_default=True)
     )
