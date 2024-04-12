34646d26f694becbe994d004387ba46317e285ba
Max Wolschlager <comms@meks.io>
2024-04-12 14:09:51 +0200

    Remove Dask and Internal Validation (#78)
    * add skip_validation to check_types wrapped func
    
    * remove dask
    
    * rename _contract_parameters, skip internal_validation
    
    * accept optional kwargs
    
    * add newline
    
    * poetry lock
    
    * ruff format
    
    * remove skip_validation in facade calls

--- ixmp4/core/decorators.py
+++ ixmp4/core/decorators.py
@@ -10,7 +10,9 @@ def check_types(func):
     checked_func = pa.check_types(func)
 
     @functools.wraps(func)
-    def wrapper(*args, **kwargs):
+    def wrapper(*args, skip_validation: bool = False, **kwargs):
+        if skip_validation:
+            return func(*args, **kwargs)
         try:
             return checked_func(*args, **kwargs)
         except PanderaSchemaError as e:
