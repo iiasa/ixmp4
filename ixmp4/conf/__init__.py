adb088e5cabb7aa9ad797e946c058d98c92fbac7
Fridolin Glatter <glatter@iiasa.ac.at>
2024-05-15 08:20:08 +0200

    Pass environment variables through json file

--- ixmp4/conf/__init__.py
+++ ixmp4/conf/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 from dotenv import load_dotenv
 
 from ixmp4.conf.settings import Settings
@@ -6,3 +8,6 @@ load_dotenv()
 # strict typechecking fails due to a bug
 # https://docs.pydantic.dev/visual_studio_code/#adding-a-default-with-field
 settings = Settings()  # type: ignore
+access_file = os.getenv("IXMP4_ACCESS_LOG")
+debug_file = os.getenv("IXMP4_DEBUG_LOG")
+error_file = os.getenv("IXMP4_ERROR_LOG")
