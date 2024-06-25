1e54d93b9351c862b315b11a9a5e072d27ef59df
Max Wolschlager <rxtx@ungut.at>
2024-06-25 14:14:47 +0200

    refactor static methods to utils module

--- ixmp4/data/db/optimization/base.py
+++ ixmp4/data/db/optimization/base.py
@@ -1,10 +1,4 @@
-from typing import Any
-
-import pandas as pd
-from sqlalchemy.orm import Mapped as Mapped
-
-from ixmp4 import db
-from ixmp4.data import abstract
+from ixmp4.data import types
 
 from ..base import BaseModel as RootBaseModel
 from ..base import (
@@ -25,57 +19,4 @@ class BaseModel(RootBaseModel, TimestampMixin):
     __abstract__ = True
     table_prefix = "optimization_"
 
-    name: Mapped[db.Name]
-
-
-class OptimizationData:
-    @staticmethod
-    def collect_indexsets_to_check(
-        columns: list[abstract.optimization.Column],
-    ) -> dict[str, Any]:
-        """Creates a {key:value} dict from linked Column.names and their
-        IndexSet.elements."""
-        collection: dict[str, Any] = {}
-        for column in columns:
-            collection[column.name] = column.indexset.elements
-        return collection
-
-    @classmethod
-    def validate_data(
-        cls, key, data: dict[str, Any], columns: list[abstract.optimization.Column]
-    ):
-        data_frame: pd.DataFrame = pd.DataFrame.from_dict(data)
-        # TODO for all of the following, we might want to create unique exceptions
-        # Could me make both more specific by specifiying missing/extra columns?
-        if len(data_frame.columns) < len(columns):
-            raise ValueError(
-                f"Data is missing for some Columns! \n Data: {data} \n "
-                f"Columns: {[column.name for column in columns]}"
-            )
-        elif len(data_frame.columns) > len(columns):
-            raise ValueError(
-                f"Trying to add data to unknown Columns! \n Data: {data} \n "
-                f"Columns: {[column.name for column in columns]}"
-            )
-
-        # We could make this more specific maybe by pointing to the missing values
-        if data_frame.isna().any(axis=None):
-            raise ValueError(
-                "The data is missing values, please make sure it "
-                "does not contain None or NaN, either!"
-            )
-        # We can make this more specific e.g. highlighting all duplicate rows via
-        # pd.DataFrame.duplicated(keep="False")
-        if data_frame.value_counts().max() > 1:
-            raise ValueError("The data contains duplicate rows!")
-
-        # Can we make this more specific? Iterating over columns; if any is False,
-        # return its name or something?
-        limited_to_indexsets = cls.collect_indexsets_to_check(columns=columns)
-        if not data_frame.isin(limited_to_indexsets).all(axis=None):
-            raise ValueError(
-                "The data contains values that are not allowed as per the IndexSets "
-                "and Columns it is constrained to!"
-            )
-
-        return data_frame.to_dict(orient="list")
+    name: types.Name
