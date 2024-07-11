93ec91fad1c94999e3d59c2958a02a2777718694
Fridolin Glatter <glatter@iiasa.ac.at>
2024-07-11 12:18:20 +0200

    Refactor to only passing run for interface simplicity

--- tutorial/transport/dantzig_model_linopy.py
+++ tutorial/transport/dantzig_model_linopy.py
@@ -1,8 +1,7 @@
 import linopy
 import pandas as pd
 
-from ixmp4.core import Equation, IndexSet, Parameter, Scalar
-from ixmp4.core import OptimizationVariable as Variable
+from ixmp4.core import Parameter, Run
 
 
 def create_parameter(
@@ -14,46 +13,43 @@ def create_parameter(
     return pd.Series(data=parameter.values, index=index, name=name)
 
 
-def create_dantzig_model(
-    i: IndexSet,
-    j: IndexSet,
-    a: Parameter,
-    b: Parameter,
-    d: Parameter,
-    f: Scalar,
-) -> linopy.Model:
+def create_dantzig_model(run: Run) -> linopy.Model:
     m = linopy.Model()
-    i_set = pd.Index(i.elements, name="Canning Plants")
-    j_set = pd.Index(j.elements, name="Markets")
-    a_parameter = create_parameter(
-        parameter=a, index=i_set, name="capacity of plant i in cases"
+    i = pd.Index(run.optimization.indexsets.get("i").elements, name="Canning Plants")
+    j = pd.Index(run.optimization.indexsets.get("j").elements, name="Markets")
+    a = create_parameter(
+        parameter=run.optimization.parameters.get("a"),
+        index=i,
+        name="capacity of plant i in cases",
     )
-    b_parameter = create_parameter(
-        parameter=b, index=j_set, name="demand at market j in cases"
+    b = create_parameter(
+        parameter=run.optimization.parameters.get("b"),
+        index=j,
+        name="demand at market j in cases",
     )
-    d_parameter = create_parameter(
-        parameter=d, index=[i_set, j_set], name="distance in thousands of miles"
+    d = create_parameter(
+        parameter=run.optimization.parameters.get("d"),
+        index=[i, j],
+        name="distance in thousands of miles",
     )
-    f_scalar = f.value
+    f = run.optimization.scalars.get("f").value
 
-    c = d_parameter * f_scalar / 1000
+    c = d * f / 1000
     c.name = "transport cost in thousands of dollars per case"
 
-    x = m.add_variables(
-        lower=0.0, coords=[i_set, j_set], name="Shipment quantities in cases"
-    )
+    x = m.add_variables(lower=0.0, coords=[i, j], name="Shipment quantities in cases")
 
     m.add_constraints(
         lhs=x.sum(dim="Markets"),
         sign="<=",
-        rhs=a_parameter,
+        rhs=a,
         name="Observe supply limit at plant i",
     )
 
     m.add_constraints(
         lhs=x.sum(dim="Canning Plants"),
         sign=">=",
-        rhs=b_parameter,
+        rhs=b,
         name="Satisfy demand at market j",
     )
 
@@ -63,14 +59,14 @@ def create_dantzig_model(
     return m
 
 
-def read_dantzig_solution(
-    model: linopy.Model, z: Variable, x: Variable, demand: Equation, supply: Equation
-) -> None:
+def read_dantzig_solution(model: linopy.Model, run: Run) -> None:
     # Handle objective
     # TODO adding fake marginals here until Variables don't require this column anymore
     # Can't add units if this column was not declared above. Better stored as Scalar
     # maybe?
-    z.add(data={"levels": [model.objective.value], "marginals": [-0.0]})
+    run.optimization.variables.get("z").add(
+        data={"levels": [model.objective.value], "marginals": [-0.0]}
+    )
 
     # Handle shipment quantities
     x_data: pd.DataFrame = model.solution.to_dataframe()
@@ -86,7 +82,7 @@ def read_dantzig_solution(
     # x_data["units"] = "cases"
     # TODO Again setting fake marginals until they are optional for variables
     x_data["marginals"] = -0.0
-    x.add(data=x_data)
+    run.optimization.variables.get("x").add(data=x_data)
 
     # The following don't seem to be typed correctly by linopy
     # Add supply data
@@ -95,7 +91,7 @@ def read_dantzig_solution(
         "levels": model.constraints["Observe supply limit at plant i"].data.rhs,  # type: ignore
         "marginals": model.constraints["Observe supply limit at plant i"].data.dual,  # type: ignore
     }
-    supply.add(data=supply_data)
+    run.optimization.equations.get("supply").add(data=supply_data)
 
     # Add demand data
     demand_data = {
@@ -103,4 +99,4 @@ def read_dantzig_solution(
         "levels": model.constraints["Satisfy demand at market j"].data.rhs,  # type: ignore
         "marginals": model.constraints["Satisfy demand at market j"].data.dual,  # type: ignore
     }
-    demand.add(data=demand_data)
+    run.optimization.equations.get("demand").add(data=demand_data)
