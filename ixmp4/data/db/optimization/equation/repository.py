1a4c439455e4f33b44cd76969e66e354729cd283
Fridolin Glatter <glatter@iiasa.ac.at>
2024-08-29 10:08:05 +0200

    Remove superfluous session.add() for equation

--- ixmp4/data/db/optimization/equation/repository.py
+++ ixmp4/data/db/optimization/equation/repository.py
@@ -184,11 +184,3 @@ class EquationRepository(
         # TODO Is there a better way to reset .data?
         equation.data = {}
         self.session.commit()
-
-    @guard("edit")
-    def remove_data(self, equation_id: int) -> None:
-        equation = self.get_by_id(id=equation_id)
-        # TODO Is there a better way to reset .data?
-        equation.data = {}
-        self.session.add(equation)
-        self.session.commit()
