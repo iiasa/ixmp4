ac3a35e03fb861bda6082909dff696136ff074b3
Fridolin Glatter <glatter@iiasa.ac.at>
2024-11-29 14:34:45 +0100

    Remove outdated comment

--- ixmp4/core/run.py
+++ ixmp4/core/run.py
@@ -4,7 +4,7 @@ from typing import ClassVar, cast
 import numpy as np
 import pandas as pd
 
-# TODO Import this from typing when dropping Python 3.11 and 3.10 (Self)
+# TODO Import this from typing when dropping Python 3.11
 from typing_extensions import TypedDict, Unpack
 
 from ixmp4.data.abstract import Model as ModelModel
