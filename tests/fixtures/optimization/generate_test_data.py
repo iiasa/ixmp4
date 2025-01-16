ab82024348ca44363a0ce9e667d190dc6476ba1a
Fridolin Glatter <glatter@iiasa.ac.at>
2025-01-16 12:04:53 +0100

    ♻️ refactor(Profiling): Limit allowed parameter.data values to 100

--- tests/fixtures/optimization/generate_test_data.py
+++ tests/fixtures/optimization/generate_test_data.py
@@ -57,19 +57,19 @@ here = Path(__file__).parent
 # NOTE always use 15 indexsets for creation, then drop unnecessary later on
 indexsets = [f"Indexset {i}" for i in range(15)]
 # small
-parameter_data = [random.sample(range(100), len(indexsets)) for x in range(100)]
-pd.DataFrame(parameter_data, columns=indexsets).drop_duplicates().to_csv(
-    here / "small/parameterdata.csv", index=False
-)
+# parameter_data = [random.sample(range(100), len(indexsets)) for x in range(100)]
+# pd.DataFrame(parameter_data, columns=indexsets).drop_duplicates().to_csv(
+#     here / "small/parameterdata.csv", index=False
+# )
 
 # medium
-parameter_data = [random.sample(range(10000), len(indexsets)) for x in range(10000)]
+parameter_data = [random.sample(range(100), len(indexsets)) for x in range(10000)]
 pd.DataFrame(parameter_data, columns=indexsets).drop_duplicates().to_csv(
     here / "medium/parameterdata.csv", index=False
 )
 
 # big
-parameter_data = [random.sample(range(1000000), len(indexsets)) for x in range(1000000)]
-pd.DataFrame(parameter_data, columns=indexsets).drop_duplicates().to_csv(
-    here / "big/parameterdata.csv", index=False
-)
+# parameter_data = [random.sample(range(100), len(indexsets)) for x in range(1000000)]
+# pd.DataFrame(parameter_data, columns=indexsets).drop_duplicates().to_csv(
+#     here / "big/parameterdata.csv", index=False
+# )
