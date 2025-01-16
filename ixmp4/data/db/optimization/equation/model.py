4a0ce2fecc1f73de9a5de67a51b69ad7533e3fca
Fridolin Glatter <glatter@iiasa.ac.at>
2025-01-16 12:03:03 +0100

    ⚡ perf(Optimization): Avoid unnecessary memory usage

--- ixmp4/data/db/optimization/equation/model.py
+++ ixmp4/data/db/optimization/equation/model.py
@@ -1,4 +1,3 @@
-import copy
 from typing import Any, ClassVar
 
 from sqlalchemy.orm import validates
@@ -28,14 +27,7 @@ class Equation(base.BaseModel):
     def validate_data(self, key: Any, data: dict[str, Any]) -> dict[str, Any]:
         if data == {}:
             return data
-        data_to_validate = copy.deepcopy(data)
-        del data_to_validate["levels"]
-        del data_to_validate["marginals"]
-        _ = utils.validate_data(
-            host=self,
-            data=data_to_validate,
-            columns=self.columns,
-        )
+        utils.validate_data(host=self, data=data, columns=self.columns)
         return data
 
     __table_args__ = (db.UniqueConstraint("name", "run__id"),)
