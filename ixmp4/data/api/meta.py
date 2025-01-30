97523bf4ecc2ced607e9d409aea7712d9ea35a7e
Max Wolschlager <rxtx@ungut.at>
2025-01-30 14:16:22 +0100

    send all values via rest api distinctly

--- ixmp4/data/api/meta.py
+++ ixmp4/data/api/meta.py
@@ -23,6 +23,10 @@ class RunMetaEntry(base.BaseModel):
     key: str
     dtype: str
     value: abstract.StrictMetaValue
+    value_int: int | None
+    value_str: str | None
+    value_float: float | None
+    value_bool: bool | None
 
 
 # TODO This is tantalizingly close to the run JsonType, but not quite there.
