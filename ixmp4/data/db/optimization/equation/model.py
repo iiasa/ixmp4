7ca6c9d96fba965316473486dbd29dcd121ea07e
Fridolin Glatter <glatter@iiasa.ac.at>
2025-02-05 08:14:30 +0100

    🐛 fix: Fix actual return type of column_names property

--- ixmp4/data/db/optimization/equation/model.py
+++ ixmp4/data/db/optimization/equation/model.py
@@ -1,4 +1,4 @@
-from typing import TYPE_CHECKING, Any, ClassVar, cast
+from typing import TYPE_CHECKING, Any, ClassVar
 
 # TODO Why do we need this only for Equations?
 if TYPE_CHECKING:
@@ -66,6 +66,7 @@ class Equation(base.BaseModel):
 
     @property
     def column_names(self) -> list[str] | None:
-        return cast(list[str], self._column_names) if any(self._column_names) else None
+        names = [name for name in self._column_names if name]
+        return names if bool(names) else None
 
     __table_args__ = (db.UniqueConstraint("name", "run__id"),)
