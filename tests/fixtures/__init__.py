6f5523f78b6947c0e239bbfba3760674d0f40f38
Max Wolschlager <rxtx@ungut.at>
2025-06-03 13:32:27 +0200

    add migration data fixtures

--- tests/fixtures/__init__.py
+++ tests/fixtures/__init__.py
@@ -1,11 +1,48 @@
+import json
+import random
+import string
+from datetime import datetime
 from pathlib import Path
+from typing import Any, cast
 
+import numpy as np
 import pandas as pd
 
 import ixmp4
 from ixmp4.core.exceptions import ProgrammingError
 
 here = Path(__file__).parent
+
+
+def json_timestamp_decoder(obj: dict[Any, Any]) -> dict[Any, Any]:
+    """
+    Hook method that takes a dictionary and returns one in which qualifying
+    have been parsed into `date` and `datetime` objects.
+    """
+
+    for key, value in obj.items():
+        if not isinstance(value, str):
+            continue
+        try:
+            obj[key] = datetime.strptime(value, "%Y-%m-%d %H:%M:%S")
+        except ValueError:
+            continue
+
+    return obj
+
+
+class MigrationFixtures:
+    """Provides fixtures for testing migrations."""
+
+    @staticmethod
+    def get_migration_data(id: str) -> list[dict[str, Any]]:
+        """Get the migration data as a list of dictionaries."""
+        with open(here / "migrations" / (id + ".json"), "r") as f:
+            return cast(
+                list[dict[str, Any]], json.load(f, object_hook=json_timestamp_decoder)
+            )
+
+    c71efc396d2b = get_migration_data("c71efc396d2b")
 
 
 class SmallIamcDataset:
@@ -99,6 +136,153 @@ class MediumIamcDataset:
     regions = pd.read_csv(here / "medium/regions.csv")
     datapoints = pd.read_csv(here / "medium/datapoints.csv")
     run_cols = ["model", "scenario", "version"]
+
+    def add_ids(self, df: pd.DataFrame) -> pd.DataFrame:
+        df.insert(0, "id", range(1, len(df) + 1))
+        return df
+
+    def table_data_from_name_column(
+        self, df: pd.DataFrame, name_col: str
+    ) -> pd.DataFrame:
+        res = pd.DataFrame({"name": df[name_col].unique()})
+        res = self.add_ids(res)
+        return res
+
+    def merge_id_column(
+        self, left: pd.DataFrame, right: pd.DataFrame, name_col: str, id_col: str
+    ) -> pd.DataFrame:
+        res = left.merge(
+            right, left_on=name_col, right_on="name", suffixes=("_left", "")
+        )
+        res = res.rename(columns={"id": id_col})
+        return res.drop(columns=["name"])
+
+    def alembic_table_data(self) -> list[dict[Any, Any]]:
+        # the things i do for 100 percent test coverage
+        models = self.table_data_from_name_column(self.datapoints, "model")
+        scenarios = self.table_data_from_name_column(self.datapoints, "scenario")
+        regions = self.table_data_from_name_column(self.datapoints, "region")
+        regions["hierarchy"] = "default"
+        units = self.table_data_from_name_column(self.datapoints, "unit")
+        variables = self.table_data_from_name_column(self.datapoints, "variable")
+        measurands = self.datapoints[["unit", "variable"]].drop_duplicates()
+        measurands = self.merge_id_column(measurands, units, "unit", "unit__id")
+        measurands = self.merge_id_column(
+            measurands, variables, "variable", "variable__id"
+        )
+        measurands = self.add_ids(measurands)
+
+        runs = self.datapoints[["model", "scenario", "version"]].drop_duplicates()
+        # join models
+        runs = self.merge_id_column(runs, models, "model", "model__id")
+        # join scenarios
+        runs = self.merge_id_column(runs, scenarios, "scenario", "scenario__id")
+        runs = self.add_ids(runs)
+
+        types = pd.Series(["INT", "FLOAT", "STR", "BOOL"])
+        n_metas = 100
+        run_metas = pd.DataFrame(
+            {
+                "run__id": runs["id"].sample(n=n_metas, replace=True).values,
+                "type": types.sample(n=n_metas, replace=True).values,
+                "key": [
+                    "".join(
+                        random.choice(string.ascii_uppercase + string.digits)
+                        for _ in range(10)
+                    )
+                    for _ in range(n_metas)
+                ],
+            }
+        )
+        run_metas["value_int"] = None
+        run_metas["value_str"] = None
+        run_metas["value_bool"] = None
+        run_metas["value_float"] = None
+        run_metas.loc[run_metas["type"] == "INT", "value_int"] = 13
+        run_metas.loc[run_metas["type"] == "STR", "value_str"] = "foo"
+        run_metas.loc[run_metas["type"] == "BOOL", "value_bool"] = False
+        run_metas.loc[run_metas["type"] == "FLOAT", "value_float"] = 0.00314
+        run_metas = self.add_ids(run_metas)
+
+        timeseries = self.datapoints[
+            ["model", "scenario", "version", "region", "variable", "unit"]
+        ].drop_duplicates()
+        # join runs
+        timeseries = timeseries.merge(
+            runs,
+            left_on=["model", "scenario", "version"],
+            right_on=["model", "scenario", "version"],
+            suffixes=("", "_run"),
+        )
+        timeseries = timeseries.rename(columns={"id": "run__id"})
+
+        timeseries = timeseries.merge(
+            measurands,
+            left_on=["unit", "variable"],
+            right_on=["unit", "variable"],
+            suffixes=("", "_measurand"),
+        )
+        timeseries = timeseries.rename(columns={"id": "measurand__id"})
+
+        measurands = measurands[["id", "unit__id", "variable__id"]]
+        runs = runs[["id", "model__id", "scenario__id", "version"]]
+        runs["is_default"] = False
+
+        timeseries = self.merge_id_column(timeseries, regions, "region", "region__id")
+        timeseries = self.add_ids(timeseries)
+        datapoints = self.datapoints.merge(
+            timeseries,
+            left_on=[
+                "model",
+                "scenario",
+                "version",
+                "region",
+                "unit",
+                "variable",
+            ],
+            right_on=[
+                "model",
+                "scenario",
+                "version",
+                "region",
+                "unit",
+                "variable",
+            ],
+            suffixes=("", "_tseries"),
+        )
+        datapoints = datapoints.rename(columns={"id": "time_series__id"})
+        datapoints = self.add_ids(datapoints)
+        datapoints = datapoints[
+            [
+                "value",
+                "type",
+                "step_category",
+                "step_year",
+                "step_datetime",
+                "time_series__id",
+                "id",
+            ]
+        ]
+        datapoints = datapoints.replace({np.nan: None})
+
+        timeseries = timeseries[["region__id", "measurand__id", "run__id", "id"]]
+
+        res = []
+        for table_name, table_data in [
+            ("model", models),
+            ("scenario", scenarios),
+            ("region", regions),
+            ("unit", units),
+            ("iamc_variable", variables),
+            ("iamc_measurand", measurands),
+            ("run", runs),
+            ("runmetaentry", run_metas),
+            ("iamc_timeseries", timeseries),
+            ("iamc_datapoint_universal", datapoints),
+        ]:
+            table_data["__tablename__"] = table_name
+            res += table_data.to_dict(orient="records")
+        return res
 
     @classmethod
     def load_regions(cls, platform: ixmp4.Platform) -> None:
