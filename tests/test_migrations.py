05420c71c35978cfc9710488b6cfd8ccd524d52c
Max Wolschlager <rxtx@ungut.at>
2025-06-03 13:32:35 +0200

    add migration tests

---
+++ tests/test_migrations.py
@@ -0,0 +1,6 @@
+from pytest_alembic.tests import (
+    test_model_definitions_match_ddl as test_model_definitions_match_ddl,
+)
+from pytest_alembic.tests import test_single_head_revision as test_single_head_revision
+from pytest_alembic.tests import test_up_down_consistency as test_up_down_consistency
+from pytest_alembic.tests import test_upgrade as test_upgrade
