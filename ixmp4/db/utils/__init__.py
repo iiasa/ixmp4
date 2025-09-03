5882e81ab0dbef04d76697eeb7141b06f1c00735
Fridolin Glatter <glatter@iiasa.ac.at>
2025-09-03 16:34:24 +0200

    ✨ feat: Provide functional helper functions for abstract optimization.revert

--- ixmp4/db/utils/__init__.py
+++ ixmp4/db/utils/__init__.py
@@ -1,8 +1,10 @@
+from collections.abc import Iterable
 from contextlib import suppress
+from copy import copy
+from functools import reduce
 from typing import TYPE_CHECKING, Any, TypeVar
 
 from sqlalchemy import inspect, sql
-from sqlalchemy.orm import Mapper
 from sqlalchemy.sql import ColumnCollection, ColumnElement
 from sqlalchemy.sql.base import ReadOnlyColumnCollection
 
@@ -15,7 +17,7 @@ JoinType = TypeVar("JoinType", bound=sql.Select[tuple["BaseModel", ...]])
 
 
 # This should not need to be Any, I think, but if I put "BaseModel" instead, various
-# things like RunMetaEntry are not recognized -> this sounds like covarianve again,
+# things like RunMetaEntry are not recognized -> this sounds like covariance again,
 # but covariant typevars are not allowed as type hints.
 def is_joined(exc: sql.Select[tuple[Any, ...]], model: type["BaseModel"]) -> bool:
     """Returns `True` if `model` has been joined in `exc`."""
@@ -30,31 +32,79 @@ def is_joined(exc: sql.Select[tuple[Any, ...]], model: type["BaseModel"]) -> boo
     return False
 
 
-def get_columns(model_class: type) -> ColumnCollection[str, ColumnElement[Any]]:
-    mapper: Mapper[Any] | None = inspect(model_class)
+def get_columns(
+    model_class: type["BaseModel"],
+) -> ReadOnlyColumnCollection[str, ColumnElement[Any]]:
+    mapper = inspect(model_class)
     if mapper is not None:
         return mapper.selectable.columns
     else:
         raise ProgrammingError(f"Model class `{model_class.__name__}` is not mapped.")
 
 
+def _maybe_add_pk_column_to_collection(
+    columns: ColumnCollection[str, ColumnElement[int]], column: ColumnElement[Any]
+) -> ColumnCollection[str, ColumnElement[int]]:
+    _columns = copy(columns)
+
+    if column.primary_key:
+        _columns.add(column)
+
+    return _columns
+
+
 def get_pk_columns(
-    model_class: type,
+    model_class: type["BaseModel"],
 ) -> ReadOnlyColumnCollection[str, ColumnElement[int]]:
-    columns: ColumnCollection[str, ColumnElement[int]] = ColumnCollection()
-    for col in get_columns(model_class):
-        if col.primary_key:
-            columns.add(col)
+    columns: ColumnCollection[str, ColumnElement[int]] = reduce(
+        _maybe_add_pk_column_to_collection, get_columns(model_class), ColumnCollection()
+    )
 
     return columns.as_readonly()
+
+
+def _maybe_add_fk_column_to_collection(
+    columns: ColumnCollection[str, ColumnElement[int]], column: ColumnElement[Any]
+) -> ColumnCollection[str, ColumnElement[int]]:
+    # TODO Does this copy deep enough?
+    _columns = copy(columns)
+
+    if len(column.foreign_keys) > 0:
+        _columns.add(column)
+
+    return _columns
 
 
 def get_foreign_columns(
-    model_class: type,
+    model_class: type["BaseModel"],
 ) -> ReadOnlyColumnCollection[str, ColumnElement[int]]:
-    columns: ColumnCollection[str, ColumnElement[int]] = ColumnCollection()
-    for col in get_columns(model_class):
-        if len(col.foreign_keys) > 0:
-            columns.add(col)
+    columns: ColumnCollection[str, ColumnElement[int]] = reduce(
+        _maybe_add_fk_column_to_collection, get_columns(model_class), ColumnCollection()
+    )
 
     return columns.as_readonly()
+
+
+def create_id_map_subquery(
+    old_exc: sql.Subquery, new_exc: sql.Subquery
+) -> sql.Subquery:
+    return (
+        sql.select(old_exc.c.id.label("old_id"), new_exc.c.id.label("new_id"))
+        .join(new_exc, old_exc.c.name == new_exc.c.name)
+        .subquery()
+    )
+
+
+def _maybe_add_column_to_collection(
+    collection: ColumnCollection[str, ColumnElement[Any]],
+    column: tuple[str, ColumnElement[Any]],
+    exclude: Iterable[str],
+) -> ColumnCollection[str, ColumnElement[Any]]:
+    _collection = copy(collection)
+
+    key = column[0]
+
+    if key not in exclude:
+        _collection.add(column[1], key=key)
+
+    return _collection
