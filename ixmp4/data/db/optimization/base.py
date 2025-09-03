fa74329c84fe137ebaa42cec92c88f8449833dc2
Fridolin Glatter <glatter@iiasa.ac.at>
2025-09-03 16:37:11 +0200

    ✨ feat: Provide actual logic for optimization.revert

--- ixmp4/data/db/optimization/base.py
+++ ixmp4/data/db/optimization/base.py
@@ -1,12 +1,19 @@
-from typing import Any, ClassVar
+from typing import Any, ClassVar, TypedDict
 
 import pandas as pd
 
+# TODO Import this from typing when dropping Python 3.11
+from typing_extensions import Unpack
+
+from ixmp4 import db
 from ixmp4.core.exceptions import IxmpError
 from ixmp4.data import types
 from ixmp4.data.abstract.annotations import HasIdFilter, HasNameFilter, HasRunIdFilter
-from ixmp4.data.db.versions.model import DefaultVersionModel
-from ixmp4.data.db.versions.repository import VersionRepository
+from ixmp4.data.db.versions.repository import (
+    RunLinkedVersionRepository,
+    VersionRepository,
+)
+from ixmp4.db import utils
 from ixmp4.db.filters import BaseFilter
 
 from .. import mixins
@@ -20,8 +27,13 @@ from ..base import (
     Lister,
     ModelType,
     Retriever,
+    RunLinkedModelType,
+    RunLinkedSelecter,
     Selecter,
     Tabulator,
+)
+from ..base import (
+    RunLinkedBaseModel as RootRunLinkedBaseModel,
 )
 
 
@@ -34,8 +46,16 @@ class BaseModel(RootBaseModel, mixins.HasCreationInfo):
     name: types.Name
 
 
+class RunLinkedBaseModel(BaseModel, RootRunLinkedBaseModel):
+    __abstract__ = True
+
+
 class EnumerateKwargs(HasIdFilter, HasNameFilter, HasRunIdFilter, total=False):
     _filter: BaseFilter
+
+
+class RevertKwargs(TypedDict, total=False):
+    run__id: int
 
 
 class Reverter(BulkDeleter[ModelType], BulkUpserter[ModelType]):
@@ -49,20 +69,16 @@ class Reverter(BulkDeleter[ModelType], BulkUpserter[ModelType]):
     }
     _columns_to_drop_for_insert = _columns_to_drop_for_update | {"id"}
 
-    # These need to be defined still on the models themselves
-    columns_to_drop_for_insert = _columns_to_drop_for_insert
-    columns_to_drop_for_update = _columns_to_drop_for_update
-
     def revert(
         self,
         transaction__id: int,
-        run__id: int,
         correct_versions: pd.DataFrame | None = None,
+        **kwargs: Unpack[RevertKwargs],
     ) -> None:
-        current_versions = self.tabulate(run_id=run__id)
+        current_versions = self.tabulate(**kwargs)
         if correct_versions is None:
             correct_versions = self.versions.tabulate(
-                transaction__id=transaction__id, run__id=run__id
+                transaction__id=transaction__id, **kwargs
             )
 
         # TODO Add a check to exit early if current == correct
@@ -81,7 +97,7 @@ class Reverter(BulkDeleter[ModelType], BulkUpserter[ModelType]):
 
         versions_to_insert = correct_versions[
             ~correct_versions["id"].isin(current_versions["id"])
-        ].drop(columns=self.columns_to_drop_for_insert)
+        ].drop(columns=self._columns_to_drop_for_insert)
 
         if not versions_to_insert.empty:
             with self.backend.event_handler.pause():
@@ -90,18 +106,35 @@ class Reverter(BulkDeleter[ModelType], BulkUpserter[ModelType]):
         # Limit updates to those differing from their existing versions
         ids_to_update = set(
             self.versions.tabulate(
-                transaction__id=transaction__id,
-                run__id=run__id,
-                valid="after_transaction",
+                transaction__id=transaction__id, valid="after_transaction", **kwargs
             )["id"]
         )
         correct_versions = correct_versions[correct_versions["id"].isin(ids_to_update)]
 
         versions_to_update = correct_versions[
             correct_versions["id"].isin(current_versions["id"])
-        ].drop(columns=self.columns_to_drop_for_update)
+        ].drop(columns=self._columns_to_drop_for_update)
 
         if not versions_to_update.empty:
             self.bulk_update(versions_to_update)
 
         self.session.commit()
+
+
+class RunLinkedReverter(
+    Reverter[RunLinkedModelType],
+    RunLinkedSelecter[RunLinkedModelType],
+):
+    versions: RunLinkedVersionRepository[Any]
+
+    def _create_id_map_subquery(
+        self, transaction__id: int, run__id: int
+    ) -> db.sql.Subquery:
+        old_items_subquery = self.versions._select_for_id_map(
+            transaction__id=transaction__id, run__id=run__id
+        ).subquery()
+        new_items_subquery = self._select_for_id_map(run__id=run__id).subquery()
+
+        return utils.create_id_map_subquery(
+            old_exc=old_items_subquery, new_exc=new_items_subquery
+        )
