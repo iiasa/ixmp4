bb05c8a81dea75f346c119d1ae440b4d15e4b4a6
Max Wolschlager <rxtx@ungut.at>
2025-10-14 11:59:13 +0200

    add specific transport dependencies

--- ixmp4/rewrite/server/deps.py
+++ ixmp4/rewrite/server/deps.py
@@ -1,14 +1,30 @@
-from fastapi import Depends, Header
+from contextlib import contextmanager
+from typing import AsyncGenerator, Generator
+
+from fastapi import Depends, Header, Path
+from sqlalchemy import orm
 from toolkit.auth.context import AuthorizationContext
-from toolkit.client.auth import SelfSignedAuth
-from toolkit.manager.client import ManagerClient
+from toolkit.manager.models import Ixmp4Instance
 from toolkit.token import PreencodedToken, verify
 
 from ixmp4.rewrite.conf import settings
-from ixmp4.rewrite.transport import DirectTransport
+from ixmp4.rewrite.conf.platforms import (
+    ManagerPlatforms,
+    PlatformConnectionInfo,
+    PlatformNotFound,
+    TomlPlatform,
+)
+from ixmp4.rewrite.exceptions import Forbidden
+from ixmp4.rewrite.transport import (
+    AuthorizedTransport,
+    DirectTransport,
+    Session,
+    cached_create_engine,
+)
 
-auth = SelfSignedAuth(settings.secret_hs256, "ixmp4")
-client = ManagerClient(str(settings.manager_url), auth=auth)
+manager_client = settings.get_manager_client()
+manager_platforms = ManagerPlatforms(manager_client)
+toml_platforms = settings.get_toml_platforms()
 
 
 async def validate_token(
@@ -26,11 +42,82 @@ async def validate_token(
 async def auth_ctx(
     token: PreencodedToken | None = Depends(validate_token),
 ) -> AuthorizationContext:
-    return AuthorizationContext(getattr(token, "user", None), client)
+    user = getattr(token, "user", None)
+    return AuthorizationContext(user, manager_client)
 
 
-async def transport(
+async def get_manager_platform(
+    platform: str = Path(),
     auth_ctx: AuthorizationContext = Depends(auth_ctx),
-) -> AuthorizationContext:
-    DirectTransport(auth_ctx)
-    return AuthorizationContext(getattr(token, "user", None), client)
+) -> Ixmp4Instance:
+    ci = manager_platforms.get_platform(platform)
+
+    if ci.dsn.startswith("http"):
+        raise PlatformNotFound(f"Platform '{ci.name}' was not found.")
+
+    auth_ctx.has_access(
+        ci,
+        raise_exc=Forbidden(
+            f"Access to platform '{platform}' denied due to insufficient permissions."
+        ),
+    )
+
+    return ci
+
+
+async def get_toml_platform(
+    platform: str = Path(include_in_schema=False),
+) -> TomlPlatform:
+    ci = toml_platforms.get_platform(platform)
+
+    if ci.dsn.startswith("http"):
+        raise PlatformNotFound(f"Platform '{ci.name}' was not found.")
+
+    return ci
+
+
+@contextmanager
+def get_session(
+    platform_ci: PlatformConnectionInfo,
+) -> Generator[orm.Session, None, None]:
+    engine = cached_create_engine(platform_ci.dsn)
+    try:
+        session = Session(bind=engine)
+        yield session
+    finally:
+        session.rollback()
+        session.close()
+
+
+async def get_manager_session(
+    platform_ci: PlatformConnectionInfo = Depends(get_manager_platform),
+) -> AsyncGenerator[orm.Session, None]:
+    with get_session(platform_ci) as session:
+        yield session
+
+
+async def get_toml_session(
+    platform_ci: PlatformConnectionInfo = Depends(get_toml_platform),
+) -> AsyncGenerator[orm.Session, None]:
+    with get_session(platform_ci) as session:
+        yield session
+
+
+async def get_direct_manager_transport(
+    session: orm.Session = Depends(get_manager_session),
+) -> DirectTransport:
+    return DirectTransport(session)
+
+
+async def get_direct_toml_transport(
+    session: orm.Session = Depends(get_toml_session),
+) -> DirectTransport:
+    return DirectTransport(session)
+
+
+async def get_authorized_transport(
+    auth_ctx: AuthorizationContext | None = Depends(auth_ctx),
+    platform: Ixmp4Instance = Depends(get_manager_platform),
+    session: orm.Session = Depends(get_manager_session),
+) -> AuthorizedTransport:
+    return AuthorizedTransport(session, auth_ctx, platform)
--- ixmp4/rewrite/server/v1.py
+++ ixmp4/rewrite/server/v1.py
@@ -1,10 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.encoders import jsonable_encoder
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
 
-from ixmp4.conf import settings
 from ixmp4.rewrite import data
+from ixmp4.rewrite.conf import settings
+from ixmp4.rewrite.exceptions import ServiceException, registry
 from ixmp4.rewrite.services import Service
 
+from .deps import get_direct_toml_transport
 from .middleware import RequestSizeLoggerMiddleware, RequestTimeLoggerMiddleware
 
 app = FastAPI(
@@ -26,6 +30,17 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
+@app.exception_handler(ServiceException)
+async def http_exception_handler(request: Request, exc: ServiceException):
+    exc_dict = registry.exception_to_response_dict(exc)
+
+    return JSONResponse(
+        content=jsonable_encoder(exc_dict),
+        status_code=exc.http_status_code,
+    )
+
+
 v1_services: list[type[Service]] = [
     data.RunMetaEntryService,
     data.ModelService,
@@ -39,4 +54,5 @@ v1_services: list[type[Service]] = [
 ]
 
 for service in v1_services:
-    router = service.build_router()
+    router = service.build_router(get_direct_toml_transport)
+    app.include_router(router)
