6c8f4aff6285b5737633c7778aaf9bc161a5edf0
Max Wolschlager <rxtx@ungut.at>
2025-11-09 17:45:37 +0100

    reimplement dataframe pydantic shims

--- ixmp4/rewrite/data/dataframe.py
+++ ixmp4/rewrite/data/dataframe.py
@@ -1,19 +1,53 @@
 from datetime import datetime
-from typing import Annotated, Any, TypedDict
+from typing import Any
 
 import pandas as pd
 import pydantic as pyd
-from pandas._libs.tslibs.nattype import NaTType
+from pydantic.json_schema import JsonSchemaValue
+from pydantic_core import CoreSchema, core_schema
+from typing_extensions import NotRequired, TypedDict
+
+
+class SerializablePandasTimestamp(pd.Timestamp):
+    @pyd.model_validator(mode="before")
+    @classmethod
+    def validate_timestamp(cls, v: Any) -> pd.Timestamp:
+        if isinstance(v, pd.Timestamp):
+            return v
+
+        s = str(v)
+        return SerializablePandasTimestamp(s)
+
+    @pyd.model_serializer(mode="plain")
+    def serialize_model(self) -> str:
+        return self.isoformat()
+
+    @classmethod
+    def __get_pydantic_core_schema__(
+        cls, source_type, handler: pyd.GetCoreSchemaHandler
+    ) -> CoreSchema:
+        return core_schema.no_info_before_validator_function(
+            cls.validate_timestamp,
+            core_schema.str_schema(),
+            serialization=core_schema.plain_serializer_function_ser_schema(
+                cls.serialize_model, return_schema=core_schema.str_schema()
+            ),
+        )
+
+    @classmethod
+    def __get_pydantic_json_schema__(
+        cls, _core_schema: CoreSchema, handler: pyd.GetJsonSchemaHandler
+    ) -> JsonSchemaValue:
+        return handler(core_schema.str_schema())
 
 
 class DataFrameDict(TypedDict):
-    index: list[int] | list[str]
-    columns: list[str]
-    dtypes: list[str]
+    index: NotRequired[list[int] | list[str] | None]
+    columns: NotRequired[list[str] | None]
+    dtypes: NotRequired[list[str] | None]
     data: list[
         list[
             bool
-            | datetime
             | int
             | float
             | str
@@ -21,21 +55,21 @@ class DataFrameDict(TypedDict):
             | list[float]
             | list[int]
             | list[str]
+            | pd.Timestamp
+            | datetime
             | None
-            | NaTType
         ]
     ]
 
 
 class DataFrameTypeAdapter(pyd.BaseModel):
-    index: list[int] | list[str] | None = pyd.Field(None)
-    columns: list[str] | None
-    dtypes: list[str] | None
+    index: list[int] | list[str] | None = None
+    columns: list[str] | None = None
+    dtypes: list[str] | None = None
     data: (
         list[
             list[
                 bool
-                | datetime
                 | int
                 | float
                 | str
@@ -43,49 +77,69 @@ class DataFrameTypeAdapter(pyd.BaseModel):
                 | list[float]
                 | list[int]
                 | list[str]
+                | SerializablePandasTimestamp
+                | datetime
                 | None
-                | NaTType
             ]
         ]
         | None
     )
 
-    model_config = pyd.ConfigDict(
-        arbitrary_types_allowed=True,
-        json_encoders={pd.Timestamp: lambda x: x.isoformat(), NaTType: lambda x: "NaT"},
-    )
 
-    @pyd.model_validator(mode="before")
+class SerializableDataFrame(pd.DataFrame):
+    @pyd.model_validator(mode="wrap")
     @classmethod
-    def validate(cls, df: pd.DataFrame | DataFrameDict) -> DataFrameDict:
-        return cls.df_to_dict(df) if isinstance(df, pd.DataFrame) else df
+    def validate_model(
+        cls, v: pd.DataFrame | DataFrameDict, handler: pyd.ModelWrapValidatorHandler
+    ) -> pd.DataFrame:
+        if isinstance(v, pd.DataFrame):
+            return v
 
-    @classmethod
-    def df_to_dict(cls, df: pd.DataFrame) -> DataFrameDict:
+        if isinstance(v, dict):
+            dtypes = v.pop("dtypes", None)
+            columns = v.get("columns", None)
+            df = pd.DataFrame(**v)
+
+            if dtypes and columns:
+                for c, dt in zip(columns, dtypes):
+                    # there seems to be a type incompatbility between StrDtypeArg and str
+                    df[c] = df[c].astype(dt)  # type: ignore[call-overload]
+
+            return df
+
+    @pyd.model_serializer(mode="wrap")
+    def serialize_model(
+        self, handler: pyd.SerializerFunctionWrapHandler
+    ) -> DataFrameDict:
         columns = []
         dtypes = []
-        for c in df.columns:
+        for c in self.columns:
             columns.append(c)
-            dtypes.append(df[c].dtype.name)
+            dtypes.append(self[c].dtype.name)
 
-        return DataFrameDict(
-            index=df.index.to_list(),
+        dict_ = DataFrameDict(
+            index=self.index.to_list(),
             columns=columns,
             dtypes=dtypes,
-            data=df.values.tolist(),
+            data=self.values.tolist(),
+        )
+        return DataFrameTypeAdapter(**dict_).model_dump()
+
+    @classmethod
+    def __get_pydantic_core_schema__(
+        cls, source_type, handler: pyd.GetCoreSchemaHandler
+    ) -> CoreSchema:
+        adapter_schema = handler(DataFrameTypeAdapter)
+        return core_schema.no_info_wrap_validator_function(
+            cls.validate_model,
+            adapter_schema,
+            serialization=core_schema.wrap_serializer_function_ser_schema(
+                cls.serialize_model, schema=core_schema.any_schema()
+            ),
         )
 
-    def to_pandas(self) -> pd.DataFrame:
-        df = pd.DataFrame(
-            index=self.index or None,
-            columns=self.columns,
-            data=self.data,
-        )
-        if self.columns and self.dtypes:
-            for c, dt in zip(self.columns, self.dtypes):
-                # there seems to be a type incompatbility between StrDtypeArg and str
-                df[c] = df[c].astype(dt)  # type: ignore[call-overload]
-        return df
-
-
-SerializableDataFrame = Annotated[pd.DataFrame, DataFrameTypeAdapter]
+    @classmethod
+    def __get_pydantic_json_schema__(
+        cls, _core_schema: CoreSchema, handler: pyd.GetJsonSchemaHandler
+    ) -> JsonSchemaValue:
+        return handler(_core_schema)
