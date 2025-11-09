7d7a9165cdf2417d4cec9b27eb76b8c05ef5e907
Max Wolschlager <rxtx@ungut.at>
2025-11-09 18:08:33 +0100

    fix total arg in ts filter

--- ixmp4/rewrite/data/iamc/timeseries/filter.py
+++ ixmp4/rewrite/data/iamc/timeseries/filter.py
@@ -5,7 +5,7 @@ from ixmp4.rewrite.data import filters as base
 from .db import TimeSeries
 
 
-class TimeSeriesFilter(base.TimeSeriesFilter, total=True):
+class TimeSeriesFilter(base.TimeSeriesFilter, total=False):
     region: Annotated[base.RegionFilter, TimeSeries.region]
     variable: Annotated[base.VariableFilter, TimeSeries.variable]
     unit: Annotated[base.UnitFilter, TimeSeries.unit]
