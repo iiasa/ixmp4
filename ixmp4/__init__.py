dd2a3b3f0ae63fb6ca1e57fbdb433a8f3c7b14c6
Max Wolschlager <rxtx@ungut.at>
2025-11-09 17:32:01 +0100

    prototype tl namespace

--- ixmp4/__init__.py
+++ ixmp4/__init__.py
@@ -1,19 +1,20 @@
 # from ixmp4.core import IndexSet as IndexSet
-# from ixmp4.core import Model as Model
 # from ixmp4.core import Parameter as Parameter
-# from ixmp4.core import Platform as Platform
-# from ixmp4.core import Region as Region
-# from ixmp4.core import Run as Run
 # from ixmp4.core import Scalar as Scalar
-# from ixmp4.core import Scenario as Scenario
 # from ixmp4.core import Table as Table
-# from ixmp4.core import Unit as Unit
 # from ixmp4.core import Variable as Variable
-# from ixmp4.core.exceptions import InconsistentIamcType as InconsistentIamcType
-# from ixmp4.core.exceptions import IxmpError as IxmpError
-# from ixmp4.core.exceptions import NotFound as NotFound
-# from ixmp4.core.exceptions import NotUnique as NotUnique
-# from ixmp4.data.abstract import DataPoint as DataPoint
+import ixmp4.rewrite.core.iamc as iamc
+import ixmp4.rewrite.core.optimization as optimization
+from ixmp4.rewrite.core.model import Model as Model
+from ixmp4.rewrite.core.platform import Platform as Platform
+from ixmp4.rewrite.core.region import Region as Region
+from ixmp4.rewrite.core.run import Run as Run
+from ixmp4.rewrite.core.scenario import Scenario as Scenario
+from ixmp4.rewrite.core.unit import Unit as Unit
+from ixmp4.rewrite.exceptions import InconsistentIamcType as InconsistentIamcType
+from ixmp4.rewrite.exceptions import Ixmp4Error as Ixmp4Error
+from ixmp4.rewrite.exceptions import NotFound as NotFound
+from ixmp4.rewrite.exceptions import NotUnique as NotUnique
 
 __version__ = "0.0.0"
 __version_tuple__ = (0, 0, 0)
