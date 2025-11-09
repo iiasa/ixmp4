ae831c4e9063b8f6d8df297f3076dcc4953838bc
Max Wolschlager <rxtx@ungut.at>
2025-11-09 18:10:18 +0100

    add variable service to iamc root module

--- ixmp4/rewrite/data/iamc/__init__.py
+++ ixmp4/rewrite/data/iamc/__init__.py
@@ -1,2 +1,3 @@
 from .datapoint.service import DataPointService as DataPointService
 from .timeseries.service import TimeSeriesService as TimeSeriesService
+from .variable.service import VariableService as VariableService
