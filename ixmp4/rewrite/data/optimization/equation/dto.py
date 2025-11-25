d91c92a0c4642595e2e5a9b4075ddcf14469d9ac
Max Wolschlager <rxtx@ungut.at>
2025-11-25 09:24:39 +0100

    add equation module

---
+++ ixmp4/rewrite/data/optimization/equation/dto.py
@@ -0,0 +1,17 @@
+from ixmp4.rewrite.data.base.dto import BaseModel, HasCreationInfo
+
+
+class Equation(BaseModel, HasCreationInfo):
+    """Optimization equation model."""
+
+    id: int
+    name: str
+    "Name of the equation."
+
+    data: dict[str, list[float] | list[int] | list[str]]
+    indexset_names: list[str] | None
+    column_names: list[str] | None
+    run__id: int
+
+    def __str__(self) -> str:
+        return f"<Variable {self.id} name={self.name}>"
