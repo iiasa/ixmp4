3852ae34dd642709eff303deec3eb4b504a20438
Max Wolschlager <rxtx@ungut.at>
2025-11-28 13:20:08 +0100

    add detailed run filter for meta entries

--- ixmp4/data/meta/filter.py
+++ ixmp4/data/meta/filter.py
@@ -1,9 +1,15 @@
 from typing import Annotated
 
 from ixmp4.data import filters as base
+from ixmp4.data.run.db import Run
 
 from .db import RunMetaEntry
 
 
+class RunFilter(base.RunFilter, total=False):
+    model: Annotated[base.ModelFilter, Run.model]
+    scenario: Annotated[base.ScenarioFilter, Run.scenario]
+
+
 class RunMetaEntryFilter(base.RunMetaEntryFilter, total=False):
-    run: Annotated[base.RunFilter, RunMetaEntry.run]
+    run: Annotated[RunFilter, RunMetaEntry.run]
