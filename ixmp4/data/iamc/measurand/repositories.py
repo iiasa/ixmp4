dfcb14b62092c0e6ec13c8811852a70c7d4942b0
Max Wolschlager <rxtx@ungut.at>
2025-12-05 13:46:35 +0100

    add MeasurandVersionFilter

--- ixmp4/data/iamc/measurand/repositories.py
+++ ixmp4/data/iamc/measurand/repositories.py
@@ -1,8 +1,8 @@
 from toolkit import db
 
-from .db import Measurand
+from .db import Measurand, MeasurandVersion
 from .exceptions import MeasurandNotFound, MeasurandNotUnique
-from .filter import MeasurandFilter
+from .filter import MeasurandFilter, MeasurandVersionFilter
 
 
 class PandasRepository(db.r.PandasRepository):
@@ -10,3 +10,10 @@ class PandasRepository(db.r.PandasRepository):
     NotUnique = MeasurandNotUnique
     target = db.r.ModelTarget(Measurand)
     filter = db.r.Filter(MeasurandFilter, Measurand)
+
+
+class VersionRepository(db.r.PandasRepository):
+    NotFound = MeasurandNotFound
+    NotUnique = MeasurandNotUnique
+    target = db.r.ModelTarget(MeasurandVersion)
+    filter = db.r.Filter(MeasurandVersionFilter, MeasurandVersion)
