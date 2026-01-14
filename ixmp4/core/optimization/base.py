315cc25c37c6731449971865197dc099ff39f08e
Max Wolschlager <rxtx@ungut.at>
2026-01-14 21:32:54 +0100

    use qualname for Run type hint

--- ixmp4/core/optimization/base.py
+++ ixmp4/core/optimization/base.py
@@ -12,20 +12,20 @@ from ixmp4.core.base import (
 )
 
 if TYPE_CHECKING:
-    from ixmp4.core.run import Run
+    import ixmp4.core.run
 
 
 class BaseOptimizationFacadeObject(BaseFacadeObject[GetByIdServiceT, DtoT]):
-    _run: "Run"
+    _run: "ixmp4.core.run.Run"
 
-    def __init__(self, backend: Backend, dto: DtoT, run: "Run"):
+    def __init__(self, backend: Backend, dto: DtoT, run: "ixmp4.core.run.Run"):
         super().__init__(backend, dto)
         self._run = run
 
 
 class BaseOptimizationServiceFacade(BaseDocsServiceFacade[KeyT, ItemT, DocsServiceT]):
-    _run: "Run"
+    _run: "ixmp4.core.run.Run"
 
-    def __init__(self, backend: Backend, run: "Run"):
+    def __init__(self, backend: Backend, run: "ixmp4.core.run.Run"):
         super().__init__(backend)
         self._run = run
