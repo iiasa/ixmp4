8775f2d01ecaf576abda8c2d80b65d074d432af1
Max Wolschlager <rxtx@ungut.at>
2026-05-07 17:05:48 +0200

    add meta facade filter

--- ixmp4/data/meta/filter.py
+++ ixmp4/data/meta/filter.py
@@ -1,7 +1,15 @@
-from typing import Annotated
+from collections.abc import Mapping, Sequence
+from typing import Annotated, Any, cast
 
 from ixmp4.data import filters as base
+from ixmp4.data.filters.facade import (
+    FilterValueTransformer,
+    convert_facade_filter,
+    make_mapping_transformer,
+)
 from ixmp4.data.run.db import Run
+from ixmp4.data.run.filter import FacadeRunFilter
+from ixmp4.data.run.filter import facade_to_data_filter as run_facade_to_data_filter
 
 from .db import RunMetaEntry
 
@@ -13,3 +21,21 @@ class RunFilter(base.RunFilter, total=False):
 
 class RunMetaEntryFilter(base.RunMetaEntryFilter, total=False):
     run: Annotated[RunFilter, RunMetaEntry.run]
+
+
+class FacadeRunMetaEntryFilter(base.RunMetaEntryFilter, total=False):
+    run: FacadeRunFilter
+
+
+FACADE_FILTER_TRANSFORMERS: dict[str, Sequence[FilterValueTransformer]] = {
+    "run": (make_mapping_transformer(run_facade_to_data_filter),),
+}
+
+
+def facade_to_data_filter(filter_values: Mapping[str, Any]) -> RunMetaEntryFilter:
+    converted = convert_facade_filter(
+        filter_values,
+        key_map={},
+        field_transformers=FACADE_FILTER_TRANSFORMERS,
+    )
+    return cast(RunMetaEntryFilter, converted)
